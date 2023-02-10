2487e59d36c08bfa521af3c458f21e4d8d6d4615
unhappystaek <oli.ryn@wp.pl>
2023-02-10 19:09:48 +0100

    starting to not be an idiot v2

--- wzm_plugin/wzm_plugin/wzm_plugin.cs
+++ wzm_plugin/wzm_plugin/wzm_plugin.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Exiled.API.Features;
 using Exiled.API.Enums;
+using Exiled.API.Interfaces;
+
+
 
 using Server = Exiled.Events.Handlers.Server;
 using Player = Exiled.Events.Handlers.Player;
@@ -18,9 +21,22 @@ namespace wzm_plugin
         public override string Author => "unhappystaek";
         public override Version Version => new Version(1, 1, 0);
 
+        public static wzm_plugin Singleton { get; private set; }
+        
 
-        //public override void OnEnable() { }
 
-        //public override void OnDisable() { }
+        public override void OnEnabled()
+        {
+            Singleton = this;
+
+            base.OnEnabled();
         }
+
+        public override void OnDisabled()
+        {
+            Singleton = null;
+
+            base.OnDisabled();
+        }
+    }
 }
